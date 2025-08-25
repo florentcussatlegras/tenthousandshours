18e3318588673ed863a2920da367f096a219ccb8
Florent Cussatlegras <florent.cussatlegras@gmail.com>
2025-08-26 00:45:54 +0200

    backup 26-08 2

--- app/topic/delete/[...slug]/layout.tsx
+++ app/topic/delete/[...slug]/layout.tsx
@@ -1,4 +1,6 @@
-import { title } from "@/components/primitives";
+"use client";
+
+import { Breadcrumb } from "@/components/breadcrumb";
 
 export default function BlogLayout({
   children,
@@ -6,8 +8,9 @@ export default function BlogLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section className="flex flex-col items-center justify-center gap-6 py-8 md:py-10">
-      <h1 className={title()}>Supprimer une catégorie de topic</h1>
+    <section className="flex flex-col items-start gap-6 py-8 md:py-10">
+      <Breadcrumb steps={['Catégories de matières', 'Suppression d\'une matière']} />
+      <h1 className="text-3xl font-bold">Supprimer une matière</h1>
       <div className="inline-block w-1/2 text-center justify-center">
         {children}
       </div>
