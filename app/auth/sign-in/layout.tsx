9fd99e1aa5814118c958e1d412e5fb2cf9fd39fd
Florent Cussatlegras <florent.cussatlegras@gmail.com>
2025-08-26 21:32:43 +0200

    update signin form

--- app/auth/sign-in/layout.tsx
+++ app/auth/sign-in/layout.tsx
@@ -1,12 +1,21 @@
+import { Hero } from "@/components/hero";
+
+const urlImageHero = '/illustration-books-and-desk.jpg';
+
 export default function SignInLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="w-full text-center justify-center">
-        {children}
+    <section className="flex items-center h-full justify-center gap-4 bg-white">
+      <div className="flex flex-row w-full h-full items-stretch justify-center flex-1/2">
+          <div className="hidden flex-1/2 items-center md:flex justify-center">
+              <Hero imageUrl={urlImageHero} imageAlt="Books and watches on a desk" />
+          </div>
+          <div className="flex-1/2 items-center flex">
+              {children}
+          </div>
       </div>
     </section>
   );
--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -6,22 +6,18 @@ import { SignInOauthButton } from "@/components/sign-in-oauth-button";
 
 export default function SignInPage() {
   return (
-    <div className="px-8 container mx-auto max-w-screen-lg">
-
-        <Card className="w-full flex flex-col items-center py-6">
-            <CardHeader className="flex flex-col">
-                <h1 className="text-4xl font-bold mb-4 text-default-600">Identification</h1>
-                <div className="flex max-w-full gap-4 items-center">
-                  <SignInOauthButton provider="google" />
-                  <SignInOauthButton provider="github" />
-                </div>
-                {/* <Divider className="mt-6" /> */}
-            </CardHeader>
-            <CardBody>
-                <SignInForm />
-            </CardBody>
-        </Card>
-
-    </div>
+      <Card className="w-2/3 flex flex-col items-center py-6 border-default-200" shadow="none">
+          <CardHeader className="flex flex-col gap-6">
+              <h1 className="text-4xl font-bold mb-4 text-default-600">Identification</h1>
+              <div className="flex max-w-full gap-6 items-center">
+                <SignInOauthButton provider="google" />
+                <SignInOauthButton provider="github" />
+              </div>
+              {/* <Divider className="mt-6" /> */}
+          </CardHeader>
+          <CardBody>
+              <SignInForm />
+          </CardBody>
+      </Card>
   );
 }
