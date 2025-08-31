94563cd35935d98165b51754453bc3c1281ed021
Florent Cussatlegras <florent.cussatlegras@gmail.com>
2025-08-31 18:46:06 +0200

    validation study session

--- app/actions/create-study-session.action.ts
+++ app/actions/create-study-session.action.ts
@@ -49,58 +49,115 @@ export async function createStudySessionAction(
 
   let studyProcess: StudyProcess;
 
-  try {
-    var slugify = require("slugify");
-
-    studyProcess = await prisma.studyProcess.findFirst({
-      where: {
-        id: result.data.studyProcessId,
-      },
-    });
-
-    if (studyProcess === null) {
-      return {
-        errors: {
-          _form: ["Cette session n'est liée à aucun apprentissage en cours."],
-        },
-      };
-    }
-
-  const objStartedAt = result.data.startedAt?.split(':');
-  const objFinishedAt = result.data.finishedAt?.split(':');
+  const objStartedAt = result.data.startedAt?.split(":");
+  const objFinishedAt = result.data.finishedAt?.split(":");
   const dateJour = new Date();
-  const dateStartedAt = new Date(dateJour.getFullYear(), dateJour.getMonth(), dateJour.getDay(), Number(objStartedAt[0]), Number(objStartedAt[1]), Number(objStartedAt[2]));
-  const dateFinishedAt = new Date(dateJour.getFullYear(), dateJour.getMonth(), dateJour.getDay(), Number(objFinishedAt[0]), Number(objFinishedAt[1]), Number(objFinishedAt[2]));
+  const dateStartedAt = new Date(
+    dateJour.getFullYear(),
+    dateJour.getMonth(),
+    dateJour.getDay(),
+    Number(objStartedAt[0]),
+    Number(objStartedAt[1]),
+    Number(objStartedAt[2])
+  );
+  const dateFinishedAt = new Date(
+    dateJour.getFullYear(),
+    dateJour.getMonth(),
+    dateJour.getDay(),
+    Number(objFinishedAt[0]),
+    Number(objFinishedAt[1]),
+    Number(objFinishedAt[2])
+  );
 
+  // const rawSql = `SELECT * FROM "public"."StudyProcess" WHERE ("${dateStartedAt}" >= "startedAt" AND "${dateStartedAt}" <= "finishedAt") OR ("${dateFinishedAt}" >= "startedAt" AND "${dateFinishedAt}" <= "finishedAt")`;
+  // console.log(rawSql);
 
-    const studySession = await prisma.studySession.create({
-      data: {
-        description: result.data.description,
-        startedAt: dateStartedAt,
-        finishedAt: dateFinishedAt,
-        totalSeconds: (dateFinishedAt.getTime() - dateStartedAt.getTime()) / 1000,
-        studyProcessId: result.data.studyProcessId,
-      },
-    });
-  } catch (err: unknown) {
-    if (err instanceof Error) {
-      
-      return {
-        errors: {
-          _form: [err.message],
-        },
-      };
-    } else {
-      return {
-        errors: {
-          _form: [
-            "Une erreur est survenue lors de la création du processus de création",
-          ],
-        },
-      };
-    }
-  }
+  studyProcess = await prisma.$queryRaw`select * from public."StudySession" 
+   WHERE ("startedAt" <=  '2025-07-31 08:15:00' AND "finishedAt" >= '2025-07-31 08:15:00')
+   OR ("startedAt" <=  '2025-07-31 10:15:00' AND "finishedAt" >= '2025-07-31 10:15:00')
+ `;
 
-  revalidateTag("studySession");
-  redirect(`/study-process/${studyProcess.slug}`);
+  console.log(studyProcess);
+
+  // try {
+  //   var slugify = require("slugify");
+
+  //   studyProcess = await prisma.studyProcess.findFirst({
+  //     where: {
+  //       id: result.data.studyProcessId,
+  //     },
+  //   });
+
+  //   if (studyProcess === null) {
+  //     return {
+  //       errors: {
+  //         _form: ["Cette session n'est liée à aucun apprentissage en cours."],
+  //       },
+  //     };
+  //   }
+
+  //   const objStartedAt = result.data.startedAt?.split(":");
+  //   const objFinishedAt = result.data.finishedAt?.split(":");
+  //   const dateJour = new Date();
+  //   const dateStartedAt = new Date(
+  //     dateJour.getFullYear(),
+  //     dateJour.getMonth(),
+  //     dateJour.getDay(),
+  //     Number(objStartedAt[0]),
+  //     Number(objStartedAt[1]),
+  //     Number(objStartedAt[2])
+  //   );
+  //   const dateFinishedAt = new Date(
+  //     dateJour.getFullYear(),
+  //     dateJour.getMonth(),
+  //     dateJour.getDay(),
+  //     Number(objFinishedAt[0]),
+  //     Number(objFinishedAt[1]),
+  //     Number(objFinishedAt[2])
+  //   );
+
+  //   studyProcess =
+  //     await prisma.$queryRaw`SELECT * FROM "public"."StudyProcess" WHERE ("${dateStartedAt}" >= "startedAt" AND "${dateStartedAt}" <= "finishedAt") OR ("${dateFinishedAt}" >= "startedAt" AND "${dateFinishedAt}" <= "finishedAt")`;
+
+  //     console.log(studyProcess);
+
+  //   if (studyProcess !== null) {
+  //     console.log('we are here');
+  //     return {
+  //       errors: {
+  //         _form: ["Cette session dans cette tranche horaire existe dèja."],
+  //       },
+  //     };
+  //   }
+
+  //   const studySession = await prisma.studySession.create({
+  //     data: {
+  //       description: result.data.description,
+  //       startedAt: dateStartedAt,
+  //       finishedAt: dateFinishedAt,
+  //       totalSeconds:
+  //         (dateFinishedAt.getTime() - dateStartedAt.getTime()) / 1000,
+  //       studyProcessId: result.data.studyProcessId,
+  //     },
+  //   });
+  // } catch (err: unknown) {
+  //   if (err instanceof Error) {
+  //     return {
+  //       errors: {
+  //         _form: [err.message],
+  //       },
+  //     };
+  //   } else {
+  //     return {
+  //       errors: {
+  //         _form: [
+  //           "Une erreur est survenue lors de la création du processus de création",
+  //         ],
+  //       },
+  //     };
+  //   }
+  // }
+
+  // revalidateTag("studySession");
+  // redirect(`/study-process/${studyProcess.slug}`);
 }
