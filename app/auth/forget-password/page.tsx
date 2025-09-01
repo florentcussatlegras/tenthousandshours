19e9fe5a8e02104036ccdf3d342a27f4efd75c81
Florent Cussatlegras <florent.cussatlegras@gmail.com>
2025-09-01 09:39:33 +0200

    validation list study session

--- app/auth/forget-password/page.tsx
+++ app/auth/forget-password/page.tsx
@@ -4,18 +4,20 @@ import { ForgetPasswordForm } from "./forget-password-form";
 import { Card, CardBody, CardHeader, Divider } from "@heroui/react";
 
 export default function ForgetPasswordPage() {
-    
   return (
-        <Card className="w-2/3 flex flex-col items-center p-6" shadow="none">
-          <CardHeader className="flex flex-col gap-2 mb-4 items-start">
-              <h1 className="text-4xl font-bold mb-4 text-default-600">Mot de passe oublié</h1>
-              <p className="text-mute-foreground">
-                    Merci de saisir votre adresse email afin de recevoir un lien pour resaisir votre mot de passe.
-                </p>
-          </CardHeader>
-          <CardBody>
-              <ForgetPasswordForm />
-          </CardBody>
-      </Card>
+    <Card className="w-2/3 flex flex-col items-center p-6" shadow="none">
+      <CardHeader className="flex flex-col gap-2 mb-4 items-start">
+        <h1 className="text-4xl font-bold mb-4 text-default-600">
+          Mot de passe oublié
+        </h1>
+        <p className="text-mute-foreground">
+          Merci de saisir votre adresse email afin de recevoir un lien pour
+          resaisir votre mot de passe.
+        </p>
+      </CardHeader>
+      <CardBody>
+        <ForgetPasswordForm />
+      </CardBody>
+    </Card>
   );
 }
