cf3e70fd9e34e68052aa3f98cef1642d4b4b3dbf
Florent Cussatlegras <florent.cussatlegras@gmail.com>
2025-09-09 16:25:45 +0200

    list study session

--- app/scheduler/page.tsx
+++ app/scheduler/page.tsx
@@ -1,9 +1,19 @@
 import Scheduler from "@/components/scheduler";
 import * as React from "react";
 import { fetchStudySessions } from "../actions/actions";
+import { Breadcrumb } from "@/components/breadcrumb";
 
 export default async function SchedulerPage() {
   return (
-      <Scheduler defaultDate={new Date()}></Scheduler>
+    <div className="w-full space-y-6">
+      <Breadcrumb
+        steps={[
+          { label: "Mes apprentissages", url: "/profile" },
+          { label: "Mes sessions de travail" },
+        ]}
+      />
+      <h1 className="text-3xl font-bold">Mes sessions de travail</h1>
+        <Scheduler defaultDate={new Date()}></Scheduler>
+    </div>
   );
 }
