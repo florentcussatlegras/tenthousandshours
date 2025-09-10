b41d068c7935d5ae605335d0910ab70577b238ec
Florent Cussatlegras <florent.cussatlegras@gmail.com>
2025-09-10 16:20:06 +0200

    calendar week 3

--- components/week-calendar.tsx
+++ components/week-calendar.tsx
@@ -11,42 +11,32 @@ export function WeekCalendar({currentDate} : {currentDate: Date}) {
     "Dimanche",
   ];
 
-  function current_week(date: Date) {
-    let yearStart = +new Date(date.getFullYear(), 0, 1);
-    let today = +new Date(date.getFullYear(),date.getMonth(),date.getDate());
-    let dayOfYear = ((today - yearStart + 1) / 86400000);
-    let week = Math.ceil(dayOfYear / 7);
-    console.log(week);
-  }
-
-  console.log('here');
-  console.log(current_week(currentDate));
-
   let i = currentDate.getDay();
   let date = currentDate;
-  let dayBeforeCurrent = [];
+  let dayBeforeCurrentInversed = [];
   while (i > 1) {
     i--;
-    console.log(i);
     const newDate = new Date(date.getTime() - 86400000);
-    console.log(newDate);
-    dayBeforeCurrent.push(newDate);
+    dayBeforeCurrentInversed.push(newDate);
     date = newDate;
   }
 
-  let i = currentDate.getDay();
-  let date = currentDate;
-  let dayBeforeCurrent = [];
-  while (i > 1) {
-    i--;
-    console.log(i);
-    const newDate = new Date(date.getTime() - 86400000);
-    console.log(newDate);
-    dayBeforeCurrent.push(newDate);
+  const dayBeforeCurrent = dayBeforeCurrentInversed.reverse();
+  dayBeforeCurrent.push(currentDate);
+
+  i = currentDate.getDay();
+  date = currentDate;
+  let dayAfterCurrent = [];
+  while (i < 7) {
+    i++;
+    const newDate = new Date(date.getTime() + 86400000);
+    dayAfterCurrent.push(newDate);
     date = newDate;
   }
 
-  console.log(dayBeforeCurrent);
+  const dayWeeks = dayBeforeCurrent.concat(dayAfterCurrent);
+
+  console.log(dayWeeks);
 
 
   return <div className="flex flex-col">
