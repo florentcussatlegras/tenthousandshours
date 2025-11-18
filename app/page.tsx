a4d7f515cc810fe779f995e7eef00b896299d1ad
Florent Cussatlegras <florent.cussatlegras@gmail.com>
2025-11-18 19:11:12 +0100

    responsive design home and navbar 3

--- app/page.tsx
+++ app/page.tsx
@@ -9,17 +9,17 @@ export default async function Home() {
 
   return (
     <section className="flex items-center h-full justify-center gap-4 bg-white dark:bg-black/90 w-full p-0 m-0">
-      <div className="flex flex-row w-full h-full items-stretch justify-between gap-8">
-        <div className="hidden xl:w-1/2 items-center xl:flex justify-center mx-auto">
+      <div className="flex flex-row w-full h-full items-stretch justify-between">
+        <div className="hidden xl:w-1/2 items-center xl:flex justify-center">
           <Hero
             imageUrl={urlImageHero}
             imageAlt="Books and watches on a desk"
           />
         </div>
 
-        <div className="w-full 2xl:w-1/2 h-full flex flex-col gap-10 items-start justify-center">
-          <div className="mx-auto flex flex-col gap-4">
-            <h1 className="text-5xl xl:text-4xl 2xl:text-5xl font-bold text-default-600 dark:text-white/90 text-wrap">
+        <div className="w-full 2xl:w-1/2 h-full flex flex-col gap-10 items-center xl:items-start justify-center ml-0 xl:ml-18 2xl:ml-0">
+          <div className="flex flex-col gap-4 xl:w-full 2xl:w-2/3">
+            <h1 className="text-3xl sm:text-4xl lg:text-5xl xl:text-5xl font-bold text-default-600 dark:text-white/90 text-wrap">
               Quel domaine souhaitez-vous{" "}
               <span className="text-sky-500">maîtriser</span> ?
             </h1>
