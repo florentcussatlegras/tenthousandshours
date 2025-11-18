39238596142f4d0cbbe143eae67b95857cb613d6
Florent Cussatlegras <florent.cussatlegras@gmail.com>
2025-11-18 19:30:37 +0100

    responsive design home and navbar 4

--- app/page.tsx
+++ app/page.tsx
@@ -18,8 +18,8 @@ export default async function Home() {
         </div>
 
         <div className="w-full 2xl:w-1/2 h-full flex flex-col gap-10 items-center xl:items-start justify-center ml-0 xl:ml-18 2xl:ml-0">
-          <div className="flex flex-col gap-4 xl:w-full 2xl:w-2/3">
-            <h1 className="text-3xl sm:text-4xl lg:text-5xl xl:text-5xl font-bold text-default-600 dark:text-white/90 text-wrap">
+          <div className="flex flex-col gap-4 sm:px-0 sm:mx-0 w-[350px] sm:w-[600px] md:w-[700px] lg:w-[950px] xl:w-full 2xl:w-3/4">
+            <h1 className="text-3xl sm:text-4xl lg:text-5xl xl:text-4xl 2xl:text-5xl font-bold text-default-600 dark:text-white/90 text-wrap">
               Quel domaine souhaitez-vous{" "}
               <span className="text-sky-500">maîtriser</span> ?
             </h1>
