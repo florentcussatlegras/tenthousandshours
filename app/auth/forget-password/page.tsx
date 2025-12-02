3b6c428d7d1ead20c45bd6350b201f1301d6dfbb
Florent Cussatlegras <florent.cussatlegras@gmail.com>
2025-12-02 11:53:19 +0100

    update sign form button 3

--- app/auth/forget-password/page.tsx
+++ app/auth/forget-password/page.tsx
@@ -7,10 +7,10 @@ export default function ForgetPasswordPage() {
   return (
     <Card className="flex flex-col items-center p-6" shadow="none">
       <CardHeader className="flex flex-col gap-2 mb-4 items-start p-0">
-        <h1 className="text-4xl font-bold mb-4 text-default-600">
+        <h1 className="text-4xl font-bold mb-4 text-default-600 p-0">
           Mot de passe oublié
         </h1>
-        <p className="text-mute-foreground">
+        <p className="text-mute-foreground p-0">
           Merci de saisir votre adresse email afin de recevoir un lien pour
           resaisir votre mot de passe.
         </p>
