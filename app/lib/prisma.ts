4197cd21791a2109cdaa7754cf1913ad92833ea3
Florent Cussatlegras <florent.cussatlegras@gmail.com>
2025-12-12 20:33:44 +0100

    change prisma client

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,8 +1,10 @@
 import { PrismaClient } from "@prisma/client";
-import { withAccelerate } from '@prisma/extension-accelerate'
 
-export const prisma = new PrismaClient({
-  accelerateUrl: process.env.DATABASE_URL,
-}).$extends(withAccelerate())
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const adapter = new PrismaPg({ 
+  connectionString: process.env.DATABASE_URL 
+});
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
