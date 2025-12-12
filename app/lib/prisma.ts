241b9129c28571bff7e7f02c2b889604ceb83c6a
Florent Cussatlegras <florent.cussatlegras@gmail.com>
2025-12-12 20:24:14 +0100

    update prismaclient

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,13 +1,10 @@
 import { PrismaClient } from "@prisma/client";
-import { withAccelerate } from "@prisma/extension-accelerate";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
-};
+import { PrismaPg } from '@prisma/adapter-pg';
 
-const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const adapter = new PrismaPg({ 
+  connectionString: process.env.DATABASE_URL 
+});
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
